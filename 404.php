ab4491f14b5de41da0edfc4077da5f727cd929cf
okijeziorek <258296567+okijeziorek@users.noreply.github.com>
2026-08-05 17:24:53 +0200

    Przebuduj motyw na edytowalne bloki

--- 404.php
+++
@@ -1,7 +0,0 @@
-<?php get_header(); ?>
-<section class="content-panel">
-    <h1><?php esc_html_e('Nie znaleziono strony', 'gun-resort-one'); ?></h1>
-    <p><?php esc_html_e('Podany adres nie prowadzi do istniejącej treści.', 'gun-resort-one'); ?></p>
-    <p><a class="button" href="<?php echo esc_url(home_url('/')); ?>"><?php esc_html_e('Wróć na stronę główną', 'gun-resort-one'); ?></a></p>
-</section>
-<?php get_footer(); ?>
